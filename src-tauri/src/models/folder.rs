2f32baf6eae77f186bde080d8636cb1e35871f6a
Saulo <saulolduarte@outlook.com>
2025-04-27 16:22:19 -0300

    feat: add models layer with folder model

---
+++ src-tauri/src/models/folder.rs
@@ -0,0 +1,35 @@
+use diesel::{Queryable, Insertable};
+use chrono::NaiveDateTime;
+use serde::{Deserialize, Serialize};
+use crate::schema::folders;
+
+#[derive(Debug, Serialize, Deserialize, Queryable, Insertable, Clone)]
+#[diesel(table_name = folders)]
+pub struct Folder {
+    pub id: Option<i32>,
+    pub name: String,
+    pub path: String,
+    pub created_at: String,
+    pub last_accessed: Option<String>,
+    pub parent_id: Option<i32>,
+}
+
+impl Folder {
+    pub fn get_created_at_datetime(&self) -> Result<NaiveDateTime, chrono::ParseError> {
+        NaiveDateTime::parse_from_str(&self.created_at, "%Y-%m-%d %H:%M:%S")
+    }
+    
+    pub fn get_last_accessed_datetime(&self) -> Option<Result<NaiveDateTime, chrono::ParseError>> {
+        self.last_accessed.as_ref().map(|date_str| {
+            NaiveDateTime::parse_from_str(date_str, "%Y-%m-%d %H:%M:%S")
+        })
+    }
+    
+    pub fn set_created_at_datetime(&mut self, dt: NaiveDateTime) {
+        self.created_at = dt.format("%Y-%m-%d %H:%M:%S").to_string();
+    }
+    
+    pub fn set_last_accessed_datetime(&mut self, dt: Option<NaiveDateTime>) {
+        self.last_accessed = dt.map(|dt| dt.format("%Y-%m-%d %H:%M:%S").to_string());
+    }
+}
