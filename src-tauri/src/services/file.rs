ddea3e2ced6d7d57d406dc75a0e89acc4bf7e3f0
saulo-duarte <saulolduarte@outlook.com>
2025-05-02 11:38:29 -0300

    feat: add prepare_new_file function to add file metadata

--- src-tauri/src/services/file.rs
+++ src-tauri/src/services/file.rs
@@ -1,28 +1,29 @@
-use chrono::Local;
 use crate::models::NewFile;
+use chrono::Local;
+use mime_guess::MimeGuess;
 
 pub fn prepare_new_file<'a>(
     name: &'a str,
-    file_type: &'a str,
-    content: &'a [u8],
+    content: Option<&'a [u8]>,
     link: Option<&'a str>,
     folder_id: i32,
     is_favorite: bool,
 ) -> Result<NewFile<'a>, String> {
-    if file_type == "Link" && link.is_none() {
-        return Err("Link is required for file type 'Link'.".to_string());
+    let is_link = link.is_some();
+    let is_file = content.map_or(false, |c| !c.is_empty());
+
+    if !is_link && !is_file {
+        return Err("You must provide either a file content or a valid link.".to_string());
     }
 
-    if file_type != "Link" && content.is_empty() {
-        return Err("File content cannot be empty.".to_string());
-    }
+    let mime_type = MimeGuess::from_path(name).first_or_octet_stream().essence_str().to_string();
 
     Ok(NewFile {
         name,
         is_favorite,
-        file_type,
+        file_type: mime_type,
         link,
-        content,
+        content: content.unwrap_or(&[]),
         folder_id,
         created_at: Local::now().naive_local().to_string(),
         updated_at: None,
