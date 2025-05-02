a23d98a602e5edd015fab7247e6cda37eb18caf6
saulo-duarte <saulolduarte@outlook.com>
2025-05-02 11:31:19 -0300

    feat: add helper functions to handle datetime

--- src-tauri/src/models/folder.rs
+++ src-tauri/src/models/folder.rs
@@ -1,7 +1,7 @@
-use diesel::{Queryable, Insertable};
-use chrono::NaiveDateTime;
-use serde::{Deserialize, Serialize};
 use crate::schema::folders;
+use chrono::NaiveDateTime;
+use diesel::{Insertable, Queryable};
+use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize, Queryable, Insertable, Clone)]
 #[diesel(table_name = folders)]
@@ -18,17 +18,17 @@ impl Folder {
     pub fn get_created_at_datetime(&self) -> Result<NaiveDateTime, chrono::ParseError> {
         NaiveDateTime::parse_from_str(&self.created_at, "%Y-%m-%d %H:%M:%S")
     }
-    
+
     pub fn get_last_accessed_datetime(&self) -> Option<Result<NaiveDateTime, chrono::ParseError>> {
-        self.last_accessed.as_ref().map(|date_str| {
-            NaiveDateTime::parse_from_str(date_str, "%Y-%m-%d %H:%M:%S")
-        })
+        self.last_accessed
+            .as_ref()
+            .map(|date_str| NaiveDateTime::parse_from_str(date_str, "%Y-%m-%d %H:%M:%S"))
     }
-    
+
     pub fn set_created_at_datetime(&mut self, dt: NaiveDateTime) {
         self.created_at = dt.format("%Y-%m-%d %H:%M:%S").to_string();
     }
-    
+
     pub fn set_last_accessed_datetime(&mut self, dt: Option<NaiveDateTime>) {
         self.last_accessed = dt.map(|dt| dt.format("%Y-%m-%d %H:%M:%S").to_string());
     }
