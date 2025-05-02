ff7a91b18da992b87eeaeafbbaeabbe0f493bba5
saulo-duarte <saulolduarte@outlook.com>
2025-05-02 11:39:48 -0300

    feat: create file tauri commands

--- src-tauri/src/commands/file_commands.rs
+++ src-tauri/src/commands/file_commands.rs
@@ -1,28 +1,78 @@
-use tauri::command;
 use crate::db::DbConnection;
-use crate::services::prepare_new_file;
-use crate::repository::add_file;
 use crate::models::File;
+use crate::repository::add_file;
+use crate::services::prepare_new_file;
+use chrono::Local;
+use tauri::command;
 
 #[command]
 pub fn add_file_command(
     name: String,
-    file_type: String,
-    content: Vec<u8>,
+    content: Option<Vec<u8>>,
     link: Option<String>,
     folder_id: i32,
     is_favorite: bool,
 ) -> Result<File, String> {
     let mut conn = DbConnection::connect_to_active_db().map_err(|e| e.to_string())?;
 
-    let new_file = prepare_new_file(
-        &name,
-        &file_type,
-        &content,
-        link.as_deref(),
-        folder_id,
-        is_favorite,
-    )?;
+    let new_file =
+        prepare_new_file(&name, content.as_deref(), link.as_deref(), folder_id, is_favorite)?;
 
     add_file(&mut conn, &new_file).map_err(|e| e.to_string())
+}
+
+#[command]
+pub fn get_all_files_command() -> Result<Vec<File>, String> {
+    let mut conn = DbConnection::connect_to_active_db().map_err(|e| e.to_string())?;
+
+    crate::repository::get_all_files(&mut conn).map_err(|e| e.to_string())
+}
+
+#[command]
+pub fn get_file_by_id_command(file_id: i32) -> Result<File, String> {
+    let mut conn = DbConnection::connect_to_active_db().map_err(|e| e.to_string())?;
+
+    crate::repository::get_file_by_id(&mut conn, file_id).map_err(|e| e.to_string())
+}
+
+#[command]
+pub fn get_files_by_folder_id_command(folder_id_param: i32) -> Result<Vec<File>, String> {
+    let mut conn = DbConnection::connect_to_active_db().map_err(|e| e.to_string())?;
+
+    crate::repository::get_files_by_folder_id(&mut conn, folder_id_param).map_err(|e| e.to_string())
+}
+
+#[command]
+pub fn update_file_command(
+    file_id: i32,
+    name: String,
+    is_favorite: bool,
+    file_type: String,
+    link: Option<String>,
+    content: Option<Vec<u8>>,
+    created_time: String,
+    folder_id: i32,
+) -> Result<File, String> {
+    let mut conn = DbConnection::connect_to_active_db().map_err(|e| e.to_string())?;
+
+    let updated_file = File {
+        id: Some(file_id),
+        name,
+        is_favorite,
+        file_type,
+        link,
+        content: content.unwrap_or(vec![]),
+        folder_id,
+        created_at: created_time,
+        updated_at: Some(Local::now().naive_local().to_string()),
+    };
+
+    crate::repository::update_file(&mut conn, file_id, updated_file).map_err(|e| e.to_string())
+}
+
+#[command]
+pub fn delete_file_command(file_id: i32) -> Result<usize, String> {
+    let mut conn = DbConnection::connect_to_active_db().map_err(|e| e.to_string())?;
+
+    crate::repository::delete_file(&mut conn, file_id).map_err(|e| e.to_string())
 }
