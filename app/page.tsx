6aab66433ab345c581af8a00362e92278f1daad4
saulo-duarte <saulolduarte@outlook.com>
2025-05-11 15:33:57 -0300

    feat: add weekly calendar to the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import WeeklyTasks from "@/components/ui/weekly-calendar";
+
 export default function Home() {
   return (
     <div className="flex items-center justify-center h-screen">
-          
+       <WeeklyTasks />   
     </div>
   );
 }
